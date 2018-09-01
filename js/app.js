ed20a9a6f170fa20b4cea686134df69a071c1fa6
Tomas Wingord <twingord@gmail.com>
2018-09-01 01:20:14 -0300

    Added function to change cards class to show and open .. still not working

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
 /*
  * Create a list that holds all of your cards
  */
- var array document.getElementsByClassName('card');
-/*
+
+ /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
   */
+/*
+  // Shuffle function from http://stackoverflow.com/a/2450976
  restart.addEventListener('click',function shuffle(array) {
      var currentIndex = array.length, temporaryValue, randomIndex;
-
      while (currentIndex !== 0) {
          randomIndex = Math.floor(Math.random() * currentIndex);
          currentIndex -= 1;
@@ -16,32 +17,20 @@
          array[currentIndex] = array[randomIndex];
          array[randomIndex] = temporaryValue;
      }
-
-     return array;);
+     return array;)
 /*  - loop through each card and create its HTML */
-for (i=0,i=<array.lenght,i++){
+/*for (i=0,i=<array.lenght,i++){
   card = array(i);
 }
 /*  - add each card's HTML to the page */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
 /*
  * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - display the card's symbol (put this functionality in another function that you call from this one)*/
+const cardSelected = document.querySelector('.card');
+cardSelected.addEventListener('click',function changeClass(){
+  cardSelected.classList.add('open');
+})
+ /*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
