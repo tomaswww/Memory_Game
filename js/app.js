47c24f08dcd8a1ebd429948fdf0fd61e787ea75b
Tomas Wingord <twingord@gmail.com>
2018-09-09 14:57:17 -0300

    CHenged clicked cards to selectedCards to unify criteria

--- js/app.js
+++ js/app.js
@@ -2,11 +2,11 @@
  * Create a list that holds all of your cards
  */
 var deckOfCards = ["diamond","diamond","paper-plane","paper-plane","bolt","bolt","cube","cube","anchor","anchor","leaf","leaf","bicycle","bicycle","bomb","bomb"];
-var clickedCards = [];
 var selectedCards = [];
-var clickedCards = [];
 var guessedCards = [];
 var openedCards = [];
+var moves = 0;
+var stars = 3;
 
 shuffle(deckOfCards);
 // - shuffle the list of cards using the provided "shuffle" method below
@@ -38,16 +38,17 @@ for (i=0,i=<array.lenght,i++){
 }
  /*  - display the card's symbol (put this functionality in another function that you call from this one)*/
 function showCard(x) {
-    if (clickedCards.length < 2) {
+    if (selectedCards.length < 2) {
         x.target.classList.add('open');
         x.target.classList.add("show");
         x.target.removeEventListener('click', showCard);
         console.log('click');
         matchCheck(x);
-        console.log(clickedCards);
+        console.log(selectedCards);
     }
-
 }
+/*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+*  - if the list already has another card, check to see if the two cards match */
 function matchCheck(y){
   selectedCards.push(y);
   if (openedCards.length<2){
@@ -56,7 +57,7 @@ function matchCheck(y){
       if (openedCards[1]===openedCards[2]){
         console.log('Its a match dude!');
         incrementMovement();
-        clickedCards.push(y);
+        selectedCards.push(y);
         guessedCards(y);
       }
       else {
@@ -67,30 +68,25 @@ function matchCheck(y){
     }
   }
 }
-
-function hideCards(z){
-}
-
+/*    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)*/
 function guessedCards(w){
   console.log('lock cards');
-  guessedCards.push(clickedCards[0]);
-  guessedCards.push(clickedCards[1]);
+  guessedCards.push(selectedCards[0]);
+  guessedCards.push(seleedCards[1]);
   console.log(guessedCards);
-  clickedCards[0].classList.add('match');
-  clickedCards[1].classList.add('match');
-  clickedCards[0].classList.remove("hide");
-  clickedCards[1].classList.remove("hide");
+  selectedCards[0].classList.add('match');
+  selectedCards[1].classList.add('match');
+  selectedCards[0].classList.remove("hide");
+  selectedCards[1].classList.remove("hide");
   clearAllArrays();
   if (lockedCards.length === cards.length) {
+  }}
+  /* + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)*/
+  function hideCards(z){
   }
-
+function clearAllArrays(){
+  selectedCards=[];
+  openedCards=[];
 }
-
-}
- /*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+/*    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)*/
+ /*    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)*/
