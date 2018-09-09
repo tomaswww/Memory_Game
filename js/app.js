d89fe6de6895c1e0c27534789dd85d8202e40ae7
Tomas Wingord <twingord@gmail.com>
2018-09-09 13:33:24 -0300

    Created list that contains all the cards

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 /*
  * Create a list that holds all of your cards
  */
+var deckOfCards = ["diamond","diamond","paper-plane","paper-plane","bolt","bolt","cube","cube","anchor","anchor","leaf","leaf","bicycle","bicycle","bomb","bomb"];
+shuffle(deckOfCards);
+
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;}
+
 
  /*
  * Display the cards on the page
@@ -8,26 +23,27 @@
   */
 /*
   // Shuffle function from http://stackoverflow.com/a/2450976
- restart.addEventListener('click',function shuffle(array) {
-     var currentIndex = array.length, temporaryValue, randomIndex;
-     while (currentIndex !== 0) {
-         randomIndex = Math.floor(Math.random() * currentIndex);
-         currentIndex -= 1;
-         temporaryValue = array[currentIndex];
-         array[currentIndex] = array[randomIndex];
-         array[randomIndex] = temporaryValue;
-     }
-     return array;)
+ restart.addEventListener('click',)
 /*  - loop through each card and create its HTML */
-/*for (i=0,i=<array.lenght,i++){
+/*
+for (i=0,i=<array.lenght,i++){
   card = array(i);
 }
 /*  - add each card's HTML to the page */
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)*/
-const cardSelected = document.querySelector('.card');
-cardSelected.addEventListener('click',function changeClass(){
+ var classname = document.getElementsByClassName("card");
+
+ var myFunction = function() {
+     classname.classList.add("open");
+ };
+
+
+
+/*
+   const cardSelected = document.getElementsByClassName('li.card');
+document.addEventListener('click',function changeClass(){
   cardSelected.classList.add('open');
 })
  /*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
